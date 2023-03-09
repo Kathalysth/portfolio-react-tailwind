7a190f4e4a7eb6cb972b36e79a4960b1c599d41a
Chris Ehigimetor <bishopsirhchris@gmail.com>
2023-03-09 11:50:32 +0100

    fix accessibilty issue socials

--- components/Footer/Socials.js
+++ components/Footer/Socials.js
@@ -9,22 +9,28 @@ function Socials() {
   return (
     <ul className="flex items-center gap-5">
       <li>
-        <a href="https://facebook.com/amby">
+        <a href="https://facebook.com/amby" aria-label="facebook url">
           <FaFacebook className="" size={30} />
         </a>
       </li>
       <li>
-        <a href="https://instagram.com/bishopsirhchris">
+        <a
+          href="https://instagram.com/bishopsirhchris"
+          aria-label="instagram url"
+        >
           <FaInstagram className="" size={30} />
         </a>
       </li>
       <li>
-        <a href="https://twitter.com/bishopsirhchris">
+        <a href="https://twitter.com/bishopsirhchris" aria-label="twitter url">
           <FaTwitter className="" size={30} />
         </a>
       </li>
       <li>
-        <a href="https://linkedin.com/in/ambassador-chris-ehigimetor">
+        <a
+          href="https://linkedin.com/in/ambassador-chris-ehigimetor"
+          aria-label="linkedin url"
+        >
           <FaLinkedinIn className="" size={30} />
         </a>
       </li>
